90b7387b2514d335250c983a6ccd8a7f84169c6a
fadeevvania <fadeevvania@mail.ru>
2023-04-17 19:04:50 +0500

    запушил с --save

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,37 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../img/logo.png'
+import OK from '../../img/OK.png';
+import VK from '../../img/VK.png';
+
 import './Header.scss'
 const Header = () => {
     return (
         <div className='Header'>
             <div className="container">
-              <Link to="/"><img src={Logo} alt="" /></Link>  
-                <p>площадь Революции,г.Челябинск</p>
-                <div className="icons">
-                    <p>1</p>
-                    <p>2</p>
+                <div className="logo">
+                    <Link to="/"><img src={Logo} alt="" /></Link>
+                </div>
+                <div className="info">
+                    <p>площадь Революции,г.Челябинск</p>
+                    <div className="icons">
+                        <img className='icon' src={OK} alt="" />
+                        <img className='icon' src={VK} alt="" />
+                    </div>
                 </div>
             </div>
             <div className="links">
-            <Link className='link' to="">
-                <h5>Маршруты</h5>
-            </Link>
-            <Link className='link' to="">
-                <h5>О нас</h5>
-            </Link>
-            <Link className='link' to="">
-                <h5>Контакты</h5>
-            </Link>
-            <Link className='link' to="">
-                <h5>Корзина</h5>
-            </Link>
-            <Link className='link' to="">
-                <h5>Избранное</h5>
-            </Link>
-            <Link className='link' to="">
-                <h5>Профиль</h5>
-            </Link>
+                <Link className='link' to="">
+                    <h5>Маршруты</h5>
+                </Link>
+                <Link className='link' to="">
+                    <h5>О нас</h5>
+                </Link>
+                <Link className='link' to="">
+                    <h5>Контакты</h5>
+                </Link>
+                <Link className='link' to="">
+                    <h5>Корзина</h5>
+                </Link>
+                <Link className='link' to="">
+                    <h5>Избранное</h5>
+                </Link>
+                <Link className='link' to="">
+                    <h5>Профиль</h5>
+                </Link>
             </div>
         </div>
     )
